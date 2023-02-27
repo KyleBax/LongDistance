06ed44b24c9fbf7dbccace8c8e634e7937f1914e
KyleBax <102880750+KyleBax@users.noreply.github.com>
2023-02-28 11:36:27 +1300

    Added a Chat class

---
+++ Chat.cs
@@ -0,0 +1,24 @@
+﻿namespace LongDistance
+{
+    internal class Chat
+    {
+        private List<string> _messagesSent;
+
+        public List<string> MessagesSent
+        {
+            get { return _messagesSent; }
+            set { _messagesSent = value; }
+        }
+
+
+        private List<string> _messagesRecieved;
+
+        public List<string> MessagesRecieved
+        {
+            get { return _messagesRecieved; }
+            set { _messagesRecieved = value; }
+        }
+
+
+    }
+}
