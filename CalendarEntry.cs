3284b8796ad9b5e26bd5755e451ae1aa7cdb1f63
KyleBax <102880750+KyleBax@users.noreply.github.com>
2023-03-07 09:02:08 +1300

    made adjustements to Chat and Calendar now calendarEntry and ChatMessage

---
+++ CalendarEntry.cs
@@ -0,0 +1,42 @@
+﻿namespace LongDistance
+{
+    internal class CalendarEntry
+    {
+        private DateTime _eventDateTime;
+
+        public DateTime EventDateTime
+        {
+            get { return _eventDateTime; }
+            set { _eventDateTime = value; }
+        }
+
+        private string _title;
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+
+        private string _details;
+
+        public string Details
+        {
+            get { return _details; }
+            set { _details = value; }
+        }
+
+        //unsure about this one, will think about this part of it further
+        private bool _specialEvent;
+
+        public bool SpecialEvent
+        {
+            get { return _specialEvent; }
+            set { _specialEvent = value; }
+        }
+
+
+
+    }
+}
---
+++ ChatMessage.cs
@@ -0,0 +1,38 @@
+﻿namespace LongDistance
+{
+    internal class ChatMessage
+    {
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value; }
+        }
+
+        private DateTime _sentTime;
+
+        public DateTime SentTime
+        {
+            get { return _sentTime; }
+            set { _sentTime = value; }
+        }
+
+        private int _senderUserID;
+
+        public int SenderUserID
+        {
+            get { return _senderUserID; }
+            set { _senderUserID = value; }
+        }
+
+        private int _receiverUserID;
+
+        public int ReceiverUserID
+        {
+            get { return _receiverUserID; }
+            set { _receiverUserID = value; }
+        }
+
+    }
+}
